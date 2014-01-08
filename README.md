949439bbe667d424ace4a489654413b7f2092062
Guillaume AMAT <guillaume.amat@informatique-libre.com>
2014-01-08 21:56:24 +0100

    First release, with mini documentation and unit tests

--- README.md
+++ README.md
@@ -1,4 +1,41 @@
-numCheck
-========
+Math.format
+===========
 
-Function to check numbers format, do some modifications, fix minimum and maximum values, etc.
+Method to check values and format numbers.
+
+
+# Usage
+
+	Math.format(34.5279, {'round': -2}); // Returns 34.53
+	Math.format(34.5279, {'round': 0}); // Returns 35
+	Math.format(34.5279, {'round': 1}); // Returns 30
+	
+	Math.format(34.5279, {'floor': -2}); // Returns 34.52
+	Math.format(34.5279, {'floor': 0}); // Returns 34
+	Math.format(34.5279, {'floor': 1}); // Returns 30
+	
+	Math.format(34.5279, {'ceil': -2}); // Returns 34.53
+	Math.format(34.5279, {'ceil': 0}); // Returns 35
+	Math.format(34.5279, {'ceil': 1}); // Returns 40
+	
+	Math.format(34.5279, {'min': 40}); // Returns 40
+	Math.format(34.5279, {'max': 30}); // Returns 30
+	
+	Math.format('a string', {'ifNaN': 'custom value'}); // Returns 'custom value'
+	Math.format('', {'ifEmpty': 'custom value'}); // Returns 'custom value'
+	Math.format(undefined, {'ifEmpty': 'custom value'}); // Returns 'custom value'
+
+
+# Unit tests
+
+Unit tests are made with the [QUnit testing framework](http://qunitjs.com). They are usable by browsing the `test/tests.html` page or using [Grunt](http://gruntjs.com).
+
+To use Grunt, install the dependancies via [NodeJS](http://nodejs.org) and launch it
+
+	npm install
+	grunt
+
+
+# Source
+
+Some [very usefull functions](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/round) come from the marvelous [Mozilla Developer Network](https://developer.mozilla.org).
---
+++ math.format.js
@@ -0,0 +1,180 @@
+(function(){
+
+	var default_options = {
+		
+		'round': false,
+		'floor': false,
+		'ceil': false,
+		'min': false,
+		'max': false,
+		'ifNaN': NaN, // Value to return if the new value is not a number
+		'ifEmpty': undefined // Value to return if the original value is empty or undefined
+	};
+
+
+	function format(value, options) {
+		
+		// Merge user options et default options
+		if (typeof(options) != 'object') {
+			
+			options = default_options;
+		}
+		else {
+			
+			for (var index in default_options) {
+				
+				if (typeof(options[index]) == 'undefined') {
+					
+					options[index] = default_options[index];
+				}
+			}
+		}
+		
+		
+		if (typeof(value) == 'undefined' || value === '') {
+			
+			return options.ifEmpty;
+		}
+		
+		value = +value;
+		
+		if (isNaN(value)) {
+			
+			return options.ifNaN;
+		}
+		
+		
+		var new_value = value;
+		
+		
+		// Minimum
+		if (options.min !== false) {
+			
+			options.min = +options.min;
+			
+			if (isNaN(options.min)) {
+				
+				return options.ifNaN;
+			}
+			
+			if (new_value < options.min) {
+				
+				new_value = options.min;
+			}
+		}
+		
+		
+		// Maximum
+		if (options.max !== false) {
+			
+			options.max = +options.max;
+			
+			if (isNaN(options.max)) {
+				
+				return options.ifNaN;
+			}
+			
+			if (new_value > options.max) {
+				
+				new_value = options.max;
+			}
+		}
+		
+		
+		// Round
+		if (options.round !== false) {
+			
+			options.round = +options.round;
+			new_value = Math.round10(new_value, options.round);
+			
+			if (isNaN(new_value)) {
+				
+				return options.ifNaN;
+			}
+		}
+		
+		
+		// Floor
+		if (options.floor !== false) {
+			
+			options.floor = +options.floor;
+			new_value = Math.floor10(new_value, options.floor);
+			
+			if (isNaN(new_value)) {
+				
+				return options.ifNaN;
+			}
+		}
+		
+		
+		// Ceil
+		if (options.ceil !== false) {
+			
+			options.ceil = +options.ceil;
+			new_value = Math.ceil10(new_value, options.ceil);
+			
+			if (isNaN(new_value)) {
+				
+				return options.ifNaN;
+			}
+		}
+		
+		
+		return new_value;
+	}
+
+	if (!Math.format) {
+		
+		Math.format = format;
+	}
+
+
+
+	// Usefull methods from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/round
+	/**
+	 * Decimal adjustment of a number.
+	 *
+	 * @param	{String}	type	The type of adjustment.
+	 * @param	{Number}	value	The number.
+	 * @param	{Integer}	exp		The exponent (the 10 logarithm of the adjustment base).
+	 * @returns	{Number}			The adjusted value.
+	 */
+	function decimalAdjust(type, value, exp) {
+		// If the exp is undefined or zero...
+		if (typeof exp === 'undefined' || +exp === 0) {
+			return Math[type](value);
+		}
+		value = +value;
+		exp = +exp;
+		// If the value is not a number or the exp is not an integer...
+		if (isNaN(value) || !(typeof exp === 'number' && exp % 1 === 0)) {
+			return NaN;
+		}
+		// Shift
+		value = value.toString().split('e');
+		value = Math[type](+(value[0] + 'e' + (value[1] ? (+value[1] - exp) : -exp)));
+		// Shift back
+		value = value.toString().split('e');
+		return +(value[0] + 'e' + (value[1] ? (+value[1] + exp) : exp));
+	}
+
+	// Decimal round
+	if (!Math.round10) {
+		Math.round10 = function(value, exp) {
+			return decimalAdjust('round', value, exp);
+		};
+	}
+	// Decimal floor
+	if (!Math.floor10) {
+		Math.floor10 = function(value, exp) {
+			return decimalAdjust('floor', value, exp);
+		};
+	}
+	// Decimal ceil
+	if (!Math.ceil10) {
+		Math.ceil10 = function(value, exp) {
+			return decimalAdjust('ceil', value, exp);
+		};
+	}
+
+})();
