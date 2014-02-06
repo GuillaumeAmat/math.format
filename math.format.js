af6a029ffbd87fb0a3ce848cc84732b51cc0793c
Guillaume AMAT <guillaume.amat@informatique-libre.com>
2014-02-06 15:26:46 +0100

    Adds a workaround to make toLocaleString works with Safari

--- math.format.js
+++ math.format.js
@@ -1,3 +1,85 @@
+/* Author: Tyler Stark 
+ * Name: Localized Numbers Patch
+ * Url: https://github.com/TheIronDeveloper/NumberToLocaleStringPatch
+ * Purpose: Patch Legacy Browsers Usage of toLocaleString
+ **/
+(function(){
+	var tempNumber = Math.PI*1000000;
+	//  German and French localized numbers do not equal eachother.
+	// If the following passes, it suggests that the locale parameter is being ignored.
+	if(tempNumber.toLocaleString('de') === tempNumber.toLocaleString('fr') ) {
+			// Helper Function to assist with formatting numbers
+			var formatNumber = function(number, delimiter, decimalDelimiter){
+				var numberString = number.toString(), 
+					splitNumber = numberString.split('.'),
+					splitFloats = '';
+
+				// Check if the number's precision is greater than the thousanths place.
+				// If so, build out the tail end of the formatted Number.
+				if(splitNumber[1] && splitNumber[1].length > 3) {
+					splitFloats = decimalDelimiter + number.toFixed(3).toString().split('.')[1];
+				}
+
+				return splitNumber[0].split( /(?=(?:\d{3})+$)/g ).join(delimiter) + splitFloats;
+			};
+			window.Number.prototype.toLocaleString = function(){
+				var locale,
+					navigatorLanguage,
+					numberPrecision;
+				
+
+				navigatorLanguage = (navigator && navigator.language) || 'en';
+				navigatorLanguage = navigatorLanguage.replace(/\-[A-Z]+$/g, '');
+				locale = arguments[0] || navigatorLanguage;
+				
+
+				switch(locale) {
+					case 'en':
+						return formatNumber(this, ',', '.');
+					case 'au':
+						return formatNumber(this, ',', '.');
+					case 'gb':
+						return formatNumber(this, ',', '.');
+					case 'fr':
+						return formatNumber(this, ' ', ',');
+					case 'de':
+						return formatNumber(this, '.', ',');
+					case 'at':
+						return formatNumber(this, ',', '.');
+					case 'fi':
+						return formatNumber(this, ' ', ',');
+					case 'nl':
+						return formatNumber(this, '.', ',');
+					case 'fi':
+						return formatNumber(this, ',', '.');
+					case 'es':
+						return formatNumber(this, '.', ',');
+					case 'da':
+						return formatNumber(this, '.', ',');
+					case 'el':
+						return formatNumber(this, '.', ',');
+					case 'et':
+						return formatNumber(this, ' ', ',');
+					case 'it':
+						return formatNumber(this, '.', ',');
+					case 'pl':
+						return formatNumber(this, ' ', ',');
+					case 'sv':
+						return formatNumber(this, ' ', ',');
+					case 'ru':
+						return formatNumber(this, ' ', ',');
+					case 'no':
+						return formatNumber(this, ',', '.');
+				}
+
+				// Failsafe scenerio
+				return this.valueOf();
+			};
+	}
+})(); // Imediately Invoke
+
+
+
 (function(){
 
 	var default_options = {
